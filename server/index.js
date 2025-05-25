fe5b16522031abeca638eb3fbef3c311db344274
VEER-RANA <ranaveer0210@gmail.com>
2025-05-25 11:55:07 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -8,7 +8,7 @@ const app = express();
 app.use(cors()); // ✅ Apply CORS middleware
 app.use(express.static(path.join(__dirname, '../client/build')));
 
-app.get('/:path(*)', (req, res) => {
+app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, '../client/build/index.html'));
 });
 
